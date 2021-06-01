084c38128cfe0b1fd8a865b96ffee7effcb292ef
Akim Kingori <akim.king12@gmail.com>
2021-06-01 12:04:11 +0300

    list rendering

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2255, color: 'green'},
+                {id: 8635, color: 'blue'},
+                {id: 8125, color: 'blue'},
+            ],
+            sizes: ['SS', 'S', 'M', 'L', 'XL', 'XXL']
         }
     }
 })
